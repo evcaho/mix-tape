7249d10ccdedb91b8f5d0c233d148a2404143b91
Evie Caldwell-Horne <evcaldwell@gmail.com>
2017-07-18 10:31:16 -0700

    bootstrap css, create method"

--- app/controllers/tapes_controller.rb
+++ app/controllers/tapes_controller.rb
@@ -1,4 +1,8 @@
 class TapesController < ApplicationController
+  def show
+  	@tape = Tape.find(params[:id])
+  end
+
   def index
   	@tapes = Tape.all
   end
@@ -6,5 +10,19 @@ class TapesController < ApplicationController
   def new
   	@tape = Tape.new
   end
+
+  def create
+  	@tape = Tape.new(tape_params)
+  	if @tape.save
+  		render 'show'
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+    def tape_params
+    	params.require(:tape).permit(:name)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root 'tapes#index'
   resources :tapes, :songs
 end
